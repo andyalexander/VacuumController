c25496d9d6f87ac6a75ac278044f99770927e204
Andy Alexander <andrew@homeemail.me.uk>
2016-04-30 21:45:42 +0100

    Correct pins for Nano

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,111 @@
 #include <Arduino.h>
 
+#define MIN_OFFSET 0.2
+#define MAX_OFFSET 4.7
+#define ADC_RESOLUTION 1023
+#define ADC_VOLTAGE 5
+#define MAX_PRESSURE 100      // 100 kpa = max; min = 0 so ignore
+
+// http://anver.com/company/reference-guides/vacuum/
+/* note - 25" mercury in vaccum is about the max the venturi will pull
+*  therefore set this as the max.  This is equal to 84.5 kpa
+*/
+
+int ANALOG_PIN = A0;  //A0
+int PUMP_PIN = 2;     //D2
+int START_PIN = 6;
+int STOP_PIN = 7;
+
+int RUNNING = 1;
+float AV_PRESSURE = 0;
+float LAST_PRESSURE = 0;
+float PRESSURE_TOLERANCE = 0.02;
+//float TARGET_PRESSURE = 84.5;
+float TARGET_PRESSURE = 1;
+
+float calc_pressure(int voltage) {
+  float temp = (float)voltage *  ((float)ADC_VOLTAGE / (float)ADC_RESOLUTION);
+  temp = (temp - MIN_OFFSET) / (MAX_OFFSET - MIN_OFFSET);
+  temp = temp * MAX_PRESSURE;
+  return temp;
+}
+
+// determine if the vacuum pump should be on
+bool vacuum_on(float pressure) {
+  float n = 10;           // number of samples to average
+  bool ret = 0;           // pump off by default
+
+  float last_average = AV_PRESSURE;
+  AV_PRESSURE = ((AV_PRESSURE * n) - LAST_PRESSURE + pressure) / n; //update av
+
+  // check if we are filling - i.e. av < current
+  if (last_average < pressure) {
+    Serial.println(">>Filling");
+    if (pressure < TARGET_PRESSURE) {   // we need to carry on sucking
+      ret = 1;
+    }
+  }
+  else {  // we aren't filling so allow a tollerance
+    Serial.println(">>Emptying");
+    if (pressure < (TARGET_PRESSURE * (1-PRESSURE_TOLERANCE))){
+      ret = 1;
+    }
+  }
+
+  Serial.println("Target:" + String(TARGET_PRESSURE, 1) + " Actual:" + String(pressure, 1));
+  return ret;
+}
+
+void check_status(){
+  int stop = digitalRead(STOP_PIN);   // remember these both have PULLUP res.
+  int start = digitalRead(START_PIN);
+
+  if (RUNNING == true && stop == 0) {
+    RUNNING = false;
+    //Serial.println(">>Stopping pump");
+  }
+  if (RUNNING == false && start == 0){
+    RUNNING = true;
+    //Serial.println(">>Starting pump");
+  }
+}
+
+int read_voltage_av(int pin){
+  int s = 0;
+  for (int i=0; i<10; i++){
+    s += analogRead(pin);
+  }
+  return s / 10;
+}
+
 void setup() {
   Serial.begin(9600);
+  pinMode(PUMP_PIN, OUTPUT);
+  //pinMode(START_PIN, INPUT_PULLUP);
+  //pinMode(STOP_PIN, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   delay(1000);
-  Serial.print("Hello world");
+
+  //check_status();                                     // see if we are running
+  int pressure_voltage = read_voltage_av(ANALOG_PIN); // read the presure av
+
+  float pressure = calc_pressure(pressure_voltage);
+  Serial.println(" - voltage: " + String(pressure_voltage) + " | pressure: " + String(pressure));
+
+  bool pump_needed = vacuum_on(pressure);             // determine if pump needed
+
+  if (pump_needed == true && RUNNING == true) {
+    Serial.println(">>Pump ON<<");
+    digitalWrite(PUMP_PIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else {
+    Serial.println(">>Pump OFF<<");
+    digitalWrite(PUMP_PIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
 }
