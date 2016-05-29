482eb3048a984fdc423b6b2deafb9076662058b2
Andy <andrew@homeemail.me.uk>
2016-05-29 18:49:48 +0100

    added timer

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 *  therefore set this as the max.  This is equal to 84.5 kpa
 */
 
+/* sensor:  pin 1 - signal; 2 = gnd; 3 = 5v */
+
 int ANALOG_PIN = A0;  //A0
 int PUMP_PIN = 2;     //D2
 int START_PIN = 6;
@@ -22,6 +24,8 @@ float LAST_PRESSURE = 0;
 float PRESSURE_TOLERANCE = 0.02;
 //float TARGET_PRESSURE = 84.5;
 float TARGET_PRESSURE = 70;
+
+unsigned long MAX_RUN_TIME = 3600;  // max run for an hour
 
 float calc_pressure(int voltage) {
   float temp = (float)voltage *  ((float)ADC_VOLTAGE / (float)ADC_RESOLUTION);
@@ -40,19 +44,19 @@ bool vacuum_on(float pressure) {
 
   // check if we are filling - i.e. av < current
   if (last_average < pressure) {
-    Serial.println(">>Filling");
+    Serial.print(" >>Filling<< ");
     if (pressure < TARGET_PRESSURE) {   // we need to carry on sucking
       ret = 1;
     }
   }
   else {  // we aren't filling so allow a tollerance
-    Serial.println(">>Emptying");
+    Serial.print(" >>Emptying<< ");
     if (pressure < (TARGET_PRESSURE * (1-PRESSURE_TOLERANCE))){
       ret = 1;
     }
   }
 
-  Serial.println("Target:" + String(TARGET_PRESSURE, 1) + " Actual:" + String(pressure, 1));
+  Serial.print("Target:" + String(TARGET_PRESSURE, 1) + " Actual:" + String(pressure, 1));
   return ret;
 }
 
@@ -89,23 +93,29 @@ void setup() {
 void loop() {
   delay(1000);
 
-  //check_status();                                     // see if we are running
-  int pressure_voltage = read_voltage_av(ANALOG_PIN); // read the presure av
+  // if we haven't exceeded the run time
+  if (millis() < (MAX_RUN_TIME * 1000)){
+      //check_status();                                     // see if we are running
+      int pressure_voltage = read_voltage_av(ANALOG_PIN); // read the presure av
 
-  float pressure = calc_pressure(pressure_voltage);
-  Serial.println(" - voltage: " + String(pressure_voltage) + " | pressure: " + String(pressure));
+      float pressure = calc_pressure(pressure_voltage);
+      //Serial.println(" - voltage: " + String(pressure_voltage) + " | pressure: " + String(pressure));
 
-  bool pump_needed = vacuum_on(pressure);             // determine if pump needed
+      bool pump_needed = vacuum_on(pressure);             // determine if pump needed
 
-  if (pump_needed == true && RUNNING == true) {
-    Serial.println(">>Pump ON<<");
-    digitalWrite(PUMP_PIN, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
-  else {
-    Serial.println(">>Pump OFF<<");
+      if (pump_needed == true && RUNNING == true) {
+        Serial.println(" >>Pump ON<< ");
+        digitalWrite(PUMP_PIN, HIGH);
+        digitalWrite(LED_BUILTIN, HIGH);
+      }
+      else {
+        Serial.println(" >>Pump OFF<< ");
+        digitalWrite(PUMP_PIN, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
+      }
+  } else {
+    Serial.println("Timer complete...");
     digitalWrite(PUMP_PIN, LOW);
-    digitalWrite(LED_BUILTIN, LOW);
   }
 
 }
